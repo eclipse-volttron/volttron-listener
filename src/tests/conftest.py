1bf744f7dc4db3168aebcebb92ab2e0fdfd77967
C. Allwardt <--user.email>
2022-07-13 22:58:51 -0700

    Added fixtures from platform wrapper. Bump project version Added integration test to project.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -5,4 +5,4 @@ p = Path(__file__)
 if p.parent.parent.parent.resolve().as_posix() not in sys.path:
     sys.path.insert(0, p.parent.parent.resolve().as_posix())
 
-print(sys.path)
+from volttrontesting.fixtures.volttron_platform_fixtures import *
