e9481ef6a2c76a745e9f8e822b6a74abb2660d4d
Craig <3979063+craig8@users.noreply.github.com>
2022-11-16 14:45:09 -0800

    move tests up

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,8 +25,9 @@
 import sys
 from pathlib import Path
 
-p = Path(__file__)
-if p.parent.parent.parent.resolve().as_posix() not in sys.path:
+path = Path(__file__).parent.parent.joinpath('src')
+
+if path.resolve() not in sys.path:
     sys.path.insert(0, p.parent.parent.resolve().as_posix())
 
 from volttrontesting.fixtures.volttron_platform_fixtures import *
